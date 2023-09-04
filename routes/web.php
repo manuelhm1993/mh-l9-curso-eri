020cddd567d9f5f3d50eceba8d702244f77d9c47
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-04 06:00:08 -0400

    Laravel es un framework php que usa front controller, lo que significa que todo parte del index, pero las rutas se encargan de determinar la página correcta que se solicita para dar una respuesta

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,24 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return "Bienvenidos al curso de Laravel 9 en El rincón de Isma";
+});
+
+Route::get('/juegos', function () {
+    return "Bienvenido a la web que listará los juegos comprados";
+});
+
+// Flujo de ejecución, se debe colocar en orden si tiene la misma estructura
+Route::get('/juegos/create', function () {
+    return "Esta es la página donde se creará el formulario para dar de alta juegos";
+});
+
+// Parámetros opcionales
+Route::get('/juegos/{nombreJuego}/{categoria?}', function ($nombreJuego, $categoria = '') {
+    $response = ($categoria === '')
+    ? "Bienvenido a la página del juego: $nombreJuego"
+    : "Bienvenido a la página del juego: {$nombreJuego} que pertenece a la categoría: {$categoria}";
+
+    return $response;
 });
